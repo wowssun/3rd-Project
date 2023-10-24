3ec50b23e8d497f32ba7ac0741bcc234b6cbc446
Johmk <129297420+Johmk@users.noreply.github.com>
2023-10-24 10:31:10 +0900

    Update create.sql

--- DB/create/create.sql
+++ DB/create/create.sql
@@ -189,7 +189,7 @@ CREATE TABLE ONECLASS
 	class_time varchar2(30) NOT NULL,
 	people_num number NOT NULL,
 	running_time number NOT NULL,
-	class_img NULL,
+	class_img DEFAULT "default.png" NOT NULL,
 	PRIMARY KEY (cno)
 );
 
