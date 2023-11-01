fe31013a393e0f3e96c2d8202066a297b03baa88
wow_dev <119738419+wowssun@users.noreply.github.com>
2023-11-01 15:43:27 +0900

    Update insert.sql

--- DB/insert/insert.sql
+++ DB/insert/insert.sql
@@ -86,4 +86,51 @@ insert into class_attach values(3, 'aaa', 'aaa', 'aaa');
 insert into class_req values(SEQ_CLASS_REQ_reqno.nextval, 3, 'aaa');
 insert into review values(seq_review_rno.nextval, 3, 'aaa', 'aaa', '2023-10-05');
 
+-- 경매
+Insert into AUC (ANO,MID,ARTNAME,ARTIST,STARTDATE,ENDDATE,PAYDATE,PRICEC,PRICEE,INTRO,ARTIMG,CONDITION,PAYSTATEMENT,BUYER,PRICES) values (21,'abc','가나다라','가나다',to_date('23/10/22','RR/MM/DD'),to_date('23/10/18','RR/MM/DD'),to_date('23/11/01','RR/MM/DD'),0,300000,'이 작품은 예시 작품입니다.','0.jpeg','경매완료','결제중','test1',0);
+Insert into AUC (ANO,MID,ARTNAME,ARTIST,STARTDATE,ENDDATE,PAYDATE,PRICEC,PRICEE,INTRO,ARTIMG,CONDITION,PAYSTATEMENT,BUYER,PRICES) values (9,'abc','ABCD','가나다',to_date('23/10/17','RR/MM/DD'),to_date('23/10/21','RR/MM/DD'),to_date('23/11/04','RR/MM/DD'),555000,1000,'두번째 예시','5.jpg','경매완료','결제중','test1',null);
+Insert into AUC (ANO,MID,ARTNAME,ARTIST,STARTDATE,ENDDATE,PAYDATE,PRICEC,PRICEE,INTRO,ARTIMG,CONDITION,PAYSTATEMENT,BUYER,PRICES) values (22,'abc','바보','배고프다',to_date('23/10/22','RR/MM/DD'),to_date('23/10/24','RR/MM/DD'),to_date('23/11/07','RR/MM/DD'),2200,1000,'두번째 예시','5.jpg','경매완료','결제중','test2',2420);
+Insert into AUC (ANO,MID,ARTNAME,ARTIST,STARTDATE,ENDDATE,PAYDATE,PRICEC,PRICEE,INTRO,ARTIMG,CONDITION,PAYSTATEMENT,BUYER,PRICES) values (43,'abc','ABCD','가나다',to_date('23/10/30','RR/MM/DD'),to_date('23/10/31','RR/MM/DD'),null,1111,1111,'수정 테스트','5.jpg','경매중','결제 미완료',null,1111);
+Insert into AUC (ANO,MID,ARTNAME,ARTIST,STARTDATE,ENDDATE,PAYDATE,PRICEC,PRICEE,INTRO,ARTIMG,CONDITION,PAYSTATEMENT,BUYER,PRICES) values (30,'abc','ㄴㄴㄴㄴ','222',to_date('23/10/18','RR/MM/DD'),to_date('23/10/24','RR/MM/DD'),to_date('23/11/07','RR/MM/DD'),7332,6666,'이 작품은 예시 작품입니다.','0.jpeg','경매완료','결제중','test3',8065.86);
+Insert into AUC (ANO,MID,ARTNAME,ARTIST,STARTDATE,ENDDATE,PAYDATE,PRICEC,PRICEE,INTRO,ARTIMG,CONDITION,PAYSTATEMENT,BUYER,PRICES) values (41,'ghi','마바사아','사아자',to_date('23/10/26','RR/MM/DD'),to_date('23/11/01','RR/MM/DD'),null,1100,1000,'이 작품은 예시 작품입니다.','1.jpeg','경매중','결제 미완료','test1',1210);
+
+-- 입찰
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (3,'test1',9,0,555000,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (4,'test1',9,0,555000,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (5,'test1',9,0,555000,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (6,'test1',9,0,555000,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (7,'test1',9,0,555000,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (8,'test1',9,0,555000,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (9,'test1',9,2222222,555000,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (11,'test1',9,2000,555000,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (12,'test1',9,0,555000,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (13,'test1',9,55000,555000,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (14,'test1',9,555000,555000,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (15,'test1',22,2000,2200,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (16,'test1',22,2000,2200,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (17,'test2',22,2200,2200,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (18,'test2',21,0,0,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (19,'test3',21,0,0,to_date('23/10/19','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (20,'test3',30,6666,7332,to_date('23/10/23','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (21,'test3',30,7332,7332,to_date('23/10/26','RR/MM/DD'),null,null);
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (23,'test5',41,1000,1100,to_date('23/10/30','RR/MM/DD'),'마바사아','1.jpeg');
+Insert into BID (BNO,MID,ANO,PRICEB,PRICEC,BIDDATE,ARTNAME,ARTIMG) values (24,'test1',41,1100,1100,to_date('23/10/30','RR/MM/DD'),'마바사아','1.jpeg');
+
+-- 문의
+Insert into INQ (RNO,MID,TITLE,DETAIL,ANSWER,WRITEDATE,UPDATEDATE,ANSWERDATE) values (3,'test1','수정 테스트','수정 테스트 내용','답변 수정 테스트',to_date('23/10/20','RR/MM/DD'),to_date('23/10/06','RR/MM/DD'),to_date('23/10/14','RR/MM/DD'));
+Insert into INQ (RNO,MID,TITLE,DETAIL,ANSWER,WRITEDATE,UPDATEDATE,ANSWERDATE) values (4,'test1','배고프다','테스트','제발 테스트 바껴라',to_date('23/10/20','RR/MM/DD'),to_date('23/10/23','RR/MM/DD'),to_date('23/10/23','RR/MM/DD'));
+Insert into INQ (RNO,MID,TITLE,DETAIL,ANSWER,WRITEDATE,UPDATEDATE,ANSWERDATE) values (5,'test1','페이징 테스트','잘되야겠지?','굿굿',to_date('23/10/23','RR/MM/DD'),to_date('23/10/21','RR/MM/DD'),to_date('23/10/12','RR/MM/DD'));
+Insert into INQ (RNO,MID,TITLE,DETAIL,ANSWER,WRITEDATE,UPDATEDATE,ANSWERDATE) values (6,'test100','css 테스트','css 테스트','답변 수정',to_date('23/10/30','RR/MM/DD'),to_date('23/10/30','RR/MM/DD'),to_date('23/10/30','RR/MM/DD'));
+Insert into INQ (RNO,MID,TITLE,DETAIL,ANSWER,WRITEDATE,UPDATEDATE,ANSWERDATE) values (7,'test100','답변완료 잘 나오나요','테스트 중',null,to_date('23/10/30','RR/MM/DD'),null,null);
+
+-- 경매결제
+Insert into PAY (PNO,ANO,PRICE,PAYDATE) values (3,22,2200,to_date('23/10/25','RR/MM/DD'));
+Insert into PAY (PNO,ANO,PRICE,PAYDATE) values (4,30,6666,to_date('23/10/25','RR/MM/DD'));
+
+
+
+
+
+
+
 
